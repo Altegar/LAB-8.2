52b1f73baf4b383556eb7637c8607fb4e158d965
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2022-04-06 19:46:27 +0300

    Добавьте файлы проекта.

---
+++ LAB-8.2/LAB-8.2.cpp
@@ -0,0 +1,32 @@
+﻿// LAB-8.2.cpp
+// Сушинський Ігор
+// Лабораторна робота №8.2
+// Опрацювання літерних рядків.
+// Варіант 18
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ukrainian");
+
+    char str[256], tmp[256], min[256];
+
+    cout << "Ввести лiтерний рядок:" << endl;
+    cin.getline(str, 256);
+
+    istringstream ist(str); // перетворює рядок у потік
+    ist >> min;
+    while (ist >> tmp)
+    {
+        if (strlen(tmp) < strlen(min))
+            strcpy(min, tmp);
+    }
+    cout << "Найкоротше слово = " << min << endl;
+
+    return 0;
+}
